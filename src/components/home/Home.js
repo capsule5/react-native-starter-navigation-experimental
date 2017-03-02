6a46ca92b77cbbe8a12def3d2a16a85f1644473f
stab <sebastien@capsule5.com>
2017-03-02 10:44:51 +0100

    color

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,7 +12,7 @@ import { navigateJumpToKey, swipeLeft, swipeRight } from '../../navigation/navig
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '$itemColors.green',
+    backgroundColor: '$itemColors.regular',
     justifyContent: 'center',
     alignItems: 'center'
   },
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -26,7 +26,15 @@ const styles = StyleSheet.create({
 
 class Navigation extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this._render = this._render.bind(this);
+    this._renderScene = this._renderScene.bind(this);
+    this._renderView = this._renderView.bind(this);
+  }
+
   animateCard(props) {
+    console.log('animateCard', props);
     const {
       layout,
       position,
@@ -46,12 +54,18 @@ class Navigation extends React.Component {
     //   outputRange: [-0.5, 1, -0.5]
     // });
 
-    // const rotateZ = position.interpolate({
-    //   inputRange: [index - 1, index, index + 1],
-    //   outputRange: ['20deg', '0deg', '-20deg']
-    // });
+    const rotateZ = position.interpolate({
+      inputRange: [index - 1, index, index + 1],
+      outputRange: ['20deg', '0deg', '-20deg']
+    });
 
     return {
+      position: 'absolute',
+      flex: 1,
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
       opacity: 1,
       transform: [
         { scale: 1 },
@@ -61,62 +75,8 @@ class Navigation extends React.Component {
     };
   }
 
-  render() {
-    const { navigationState, backAction } = this.props;
 
-
-    return (
-
-      // Redux is handling the reduction of our state for us. We grab the navigationState
-      // we have in our Redux store and pass it directly to the <NavigationTransitioner />.
-      <NavigationTransitioner
-        configureTransition={() => {
-          return {
-            duration: 500,
-            easing: Easing.out(Easing.exp),
-            timing: Animated.timing
-          };
-        }}
-        navigationState={navigationState}
-        style={styles.container}
-        render={(props) => {
-          const panHandlers = NavigationPagerPanResponder.forHorizontal({
-            ...props,
-            onNavigateBack: () => { return this.props.swipeRight(); },
-            onNavigateForward: () => { return this.props.swipeLeft(); }
-          });
-
-          return (
-            // This mimics the same type of work done in a NavigationCardStack component
-            <View style={styles.container}>
-              <NavigationCard
-                // <NavigationTransitioner>'s render method passes `navigationState` as a
-                // prop to here, so we expand it plus other props out in <NavigationCard>.
-                {...props}
-                // Transition animations are determined by the StyleInterpolators. Here we manually
-                // override the default horizontal style interpolator that gets applied inside of
-                // NavigationCard for a vertical direction animation if we are showing a modal.
-                // (Passing undefined causes the default interpolator to be used in NavigationCard.)
-                style={props.scene.route.key === 'Modal' ?
-                  NavigationCard.CardStackStyleInterpolator.forVertical(props) :
-                  this.animateCard(props)
-                }
-                // onNavigateBack={backAction}
-                // By default a user can swipe back to pop from the stack. Disable this for modals.
-                // Just like for style interpolators, returning undefined lets NavigationCard override it.
-                panHandlers={props.scene.route.key === 'Modal' ? null : panHandlers}
-                renderScene={this._renderScene}
-                key={props.scene.route.key}
-              />
-
-            </View>
-          );
-        }}
-      />
-    );
-  }
-
-  _renderScene({ scene }) {
+  _renderView({ scene }) {
 
     const { route } = scene;
 
@@ -131,6 +91,65 @@ class Navigation extends React.Component {
         return <Home />;
     }
   }
+
+  _renderScene(sceneProps) {
+    const panHandlers = NavigationPagerPanResponder.forHorizontal({
+      ...sceneProps,
+      onNavigateBack: () => { return this.props.swipeRight(); },
+      onNavigateForward: () => { return this.props.swipeLeft(); }
+    });
+
+    return (
+      <Animated.View
+        style={this.animateCard(sceneProps)}
+        {...panHandlers}
+        key={`${sceneProps.scene.key}scene`}
+      >
+        {this._renderView(sceneProps)}
+      </Animated.View>
+    );
+  }
+
+  _render(transitionProps) {
+    const scenes = transitionProps.scenes.map((scene) => {
+      const sceneProps = {
+        ...transitionProps,
+        scene
+      };
+
+      return this._renderScene(sceneProps);
+    });
+
+    return (
+      <View style={{ flex: 1 }}>
+        {scenes}
+      </View>
+    );
+  }
+
+  render() {
+    const { navigationState, backAction } = this.props;
+
+    return (
+
+      // Redux is handling the reduction of our state for us. We grab the navigationState
+      // we have in our Redux store and pass it directly to the <NavigationTransitioner />.
+      <NavigationTransitioner
+        configureTransition={() => {
+          return {
+            duration: 500,
+            easing: Easing.out(Easing.back(2)),
+            timing: Animated.timing
+          };
+        }}
+        navigationState={navigationState}
+        style={styles.container}
+        render={this._render}
+      />
+    );
+  }
+
+
 }
 
 Navigation.propTypes = {
